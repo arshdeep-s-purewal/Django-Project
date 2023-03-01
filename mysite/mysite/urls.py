dc5c831e275809a0900a2d2d3e419b3a86fd5d42
Arshdeep Singh <arshdeep.singh.1@nsi.lab>
2023-03-01 17:27:11 +0530

    UPdate in models and viewa

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from blog.views import index
 urlpatterns = [
+    path('index/', index),
     path('admin/', admin.site.urls),
 ]
